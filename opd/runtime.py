2a0d3b3aaef18ba640820f9d2bef276e5bac2ae8
Bart Thate <bthate@dds.nl>
2024-02-15 13:37:23 +0100

    add runtime

---
+++ opd/runtime.py
@@ -0,0 +1,21 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C,R,W0201
+
+
+"runtime"
+
+
+import os
+
+
+from .configs import Config
+from .storage import Storage
+
+
+Cfg         = Config()
+Cfg.mod     = "cmd,mod"
+Cfg.name    = __file__.split(os.sep)[-2]
+Cfg.wd      = os.path.expanduser(f"~/.{Cfg.name}")
+Cfg.pidfile = os.path.join(Cfg.wd, f"{Cfg.name}.pid")
+Storage.wd  = Cfg.wd
