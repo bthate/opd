6cb1b7d64eb1719c222a86aa6d436c8bfbdc74e5
Bart Thate <bthate@dds.nl>
2024-02-21 17:08:00 +0100

    enable/disable

---
+++ opd/modules/ena.py
@@ -0,0 +1,50 @@
+# This file is placed in the Public Domain.
+#
+#
+
+
+"enable/disable"
+
+
+import sys
+
+
+from ..excepts import Error
+from ..scanner import scan
+
+
+from .. import modules
+
+
+def __dir__():
+    return (
+       'dis',
+       'ena'
+    ) 
+
+
+def dis(event):
+    if not event.args:
+        mods = ",".join(dir(modules))
+        event.reply(f"disable {mods}")
+        return
+    what = event.args[0]
+    mod = getattr(sys.modules, what, None)
+    if mod:
+        try:
+            mod.shutdown()
+        except Exception as ex:
+            Error.add(mod)
+
+
+def ena(event):
+    if not event.args:
+        mods = ",".join(dir(modules))
+        event.reply(f"enable {mods}")
+        return
+    what = event.args[0]
+    try:
+        init = event.args[1]
+    except IndexError:
+        init = False
+    scan(modules, what, init)
