6b271e72e0a6fd96e10f38e0e67f28310cd88287
Bart Thate <bthate@dds.nl>
2025-02-19 20:00:16 +0100

    readd package

---
+++ opd/modules/dbg.py
@@ -0,0 +1,14 @@
+# This file is placed in the Public Domain.
+
+
+"debug"
+
+
+def dbg(event):
+    raise Exception("yo!")
+
+
+def __dir__():
+    return (
+        'dbbg',
+    )
---
+++ opd/modules/slg.py
@@ -0,0 +1,20 @@
+# This file is placed in the Public Domain.
+
+
+"slogan"
+
+
+TXT = """By law, using poison, destructing in whole or in part
+
+Elderly Handicapped Criminals Wicked
+
+@KarimKhanQC reconsider OTP-CR-117/19 4/3/2019
+
+http://bit.ly/gcid3"""
+
+
+TXT2 = "You have jurisdiction over these crimes, esp. if avoidance through not protected groups. @KarimKhanQC reconsider OTP-CR-117/19 - 4/3/2019 - http://bit.ly/gcid3"
+
+
+def slg(event):
+    event.reply(TXT2)
