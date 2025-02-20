03922b634ea2843179d0a487baadc86db7a3c533
Bart Thate <bthate@dds.nl>
2025-02-20 14:38:08 +0100

    add control

---
+++ opd/lookups.py
@@ -0,0 +1,25 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "dpl": "opd.modules.rss",
+    "err": "opd.modules.err",
+    "exp": "opd.modules.rss",
+    "flt": "opd.modules.flt",
+    "fnd": "opd.modules.fnd",
+    "imp": "opd.modules.rss",
+    "log": "opd.modules.log",
+    "mod": "opd.modules.mod",
+    "nme": "opd.modules.rss",
+    "req": "opd.modules.req",
+    "res": "opd.modules.rss",
+    "rss": "opd.modules.rss",
+    "slg": "opd.modules.slg",
+    "syn": "opd.modules.rss",
+    "thr": "opd.modules.thr",
+    "upt": "opd.modules.upt",
+    "ver": "opd.modules.upt"
+}
